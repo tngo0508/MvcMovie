27bceccf7510e6abfc9fb5f8139fe8c7a3d2faf3
Thomas Ngo <tngo0508@gmail.com>
2022-01-18 12:59:46 -0800

    Adding a controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is " + numTimes);
+        //}
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
